8200dc1bd7a6344bebbe663814fd1a6d81235f59
rmparanhos <raffaelmp@id.uff.br>
2016-10-14 00:33:36 -0300

    2.0
    Quase tudo funciona, só falta encontrar um semaforo pq o while da linha
    39 ta dando starvation nesse processo. O sleep da linha 32 força a troca
    do processo e tudo funciona

--- escritoresLeitores.c
+++ escritoresLeitores.c
@@ -4,58 +4,95 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int readcount,temLeitores; 
+int temLeitores,temEscritores; 
 sem_t escritores, leitores;
 
-void *leitoresMet(void *arg) { 
-
+void *leitor(void *arg) { 
 	while (1){ 
-		sem_wait(&escritores);
-		temLeitores = 1;
-		//READUNIT(); 
-		printf("leitores\n");
-		fflush(stdout);
-		temLeitores = 0;
-		sleep(1);
-		sem_post(&escritores); 
-		} 
+		temLeitores ++;
+		if(temEscritores){
+			sem_wait(&escritores);
+			printf("leitores ini %d\n",arg);
+			fflush(stdout);
+			//READUNIT(); 
+			sleep(1);
+			printf("leitores fim %d\n",arg);
+			fflush(stdout);
+			sem_post(&escritores); 
+		}
+		else{
+			printf("leitores ini %d\n",arg);
+			fflush(stdout);
+			//READUNIT();
+			sleep(1);
+			printf("leitores fim %d\n",arg);
+			fflush(stdout);
+		}
+		temLeitores --; //errro está aqui
+		sleep(1); //forçando interrupçao, tudo funciona, preciso usar um semafaro no lugar desse sleep.
+	}
 } 
 
-void *escritoresMet(void *arg){ 
+void *escritor(void *arg){ 
  
 	while (1){ 
+		while(temLeitores);
 		sem_wait(&escritores); //Apenas um escritor de cada vez, os outros processos escritores ficam presos aqui
-		while (temLeitores);
-		//temEscritores = 1; //Se existe um escritor, ngm pode ler
-		//WRITEUNIT(); 
-		printf("escritores\n");
+		temEscritores = 1; //Se existe um escritor, ngm pode ler
+		printf("escritores ini %d\n",arg);
 		fflush(stdout);
-		//temEscritores = 0;
-		sem_post(&escritores); 
+		//WRITEUNIT(); 
+		sleep(1); 
+		fflush(stdout);
+		printf("escritores fim %d\n",arg);
+		sem_post(&escritores);
+	//	sem_post(&leitores);
+		temEscritores = 0;
 	}
 }
 
 void main() { 
 	pthread_t threadLeitor;
+	pthread_t threadLeitor2;
 	pthread_t threadEscritor;
-	readcount = 0;
+	pthread_t threadEscritor2;
 	temLeitores = 0;
+	temEscritores = 0;
+	int n = 1, m = 2;
 	sem_init(&escritores, 0, 1);  
-	if(pthread_create(&threadLeitor,NULL,leitoresMet,NULL)){
+	sem_init(&leitores, 0, 1);
+	if(pthread_create(&threadLeitor,NULL,leitor,(void*)n)){
 		printf("error creating thread leitor ");
 		abort();
 	}
-	if(pthread_create(&threadEscritor,NULL,escritoresMet,NULL)){
+	if(pthread_create(&threadLeitor2,NULL,leitor,(void*)m)){
+		printf("error creating thread leitor2");
+		abort();
+	}
+	if(pthread_create(&threadEscritor,NULL,escritor,(void*)n)){
 		printf("error creating thread escritor");
+		abort();
+	}
+	if(pthread_create(&threadEscritor2,NULL,escritor,(void*)m)){
+		printf("error creating thread escritor2");
 		abort();
 	}
 	if(pthread_join(threadLeitor,NULL)){
 		printf("error joining thread.");
 		abort();
 	}
+	if(pthread_join(threadLeitor2,NULL)){
+		printf("error joining thread;");
+		abort();
+	}
 	if(pthread_join(threadEscritor,NULL)){
 		printf("error joining thread;");
 		abort();
 	}
+	if(pthread_join(threadEscritor,NULL)){
+		printf("error joining thread;");
+		abort();
+	}
+	
 }
 
